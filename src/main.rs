cd42d7a894a12c8ab5f491385e25c3cc921c9ce5
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-06-05 09:54:40 +0200

    add loop over numbers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use nanoserde::{DeJson};
+use nanoserde::DeJson;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -14,31 +14,46 @@ fn main() {
         url: String,
     }
 
-    if let Ok(lines) = read_lines("cdx-00000") {
+    if let Ok(lines) = read_lines("urls.txt") {
         // Consumes the iterator, returns an (Optional) String
 
         let mut url_list = Vec::new();
 
         for line in lines.map_while(Result::ok).enumerate() {
+            // extract the json object from the cdx(j) line
             let index_json_line: &str = line.1.splitn(3, " ").nth(2).unwrap();
 
-            let test: CDXIndex = DeJson::deserialize_json(index_json_line).unwrap();
+            // Deserialse json to extract just the url
+            let index: CDXIndex = DeJson::deserialize_json(index_json_line).unwrap();
 
-            url_list.push(test.url);
+            println!("{}", index.url);
+
+            url_list.push(index.url);
 
             // now write out to a bufrwiter
 
-            if line.0 == 100 {
-                break;
-            }
-        };
+            // if line.0 == 100 {
+            //     break;
+            // }
+        }
 
         let duplicated_list = url_list.len();
+        // deduplicate the list
         url_list.dedup();
         let unduplicated_list_size = url_list.len();
 
-        println!("duplicated {duplicated_list} unduplicated {unduplicated_list_size}");
+        println!(
+            "duplicated {duplicated_list} unduplicated {unduplicated_list_size}\n\nNow convert list to numbers"
+        );
 
+        for url in url_list {
+            let url_length = url.len();
+            println!("{url_length}");
+        }
+
+        // next we want to group this into a table which lookes like a list of tuples:
+        // [(1,3),(2,5),(3,15)] etc?
+        // match
     }
 }
 
