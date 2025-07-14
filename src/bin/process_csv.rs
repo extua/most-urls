be00a1ce092e0beb6f8c4f44ba67956a3a664a93
Pierre Marshall <pierre.marshall@gmail.com>
2025-07-14 10:14:42 +0100

    feat: aggregation of dataframe

--- src/bin/process_csv.rs
+++ src/bin/process_csv.rs
@@ -1,11 +1,11 @@
+use std::fs::File;
 use polars::prelude::*;
 
 fn main() {
-
     let csv_schema = Schema::from_iter(vec![
-        Field::new("raw characters".into(), DataType::UInt64),
-        Field::new("i18n characters".into(), DataType::UInt64),
-        Field::new("status code".into(), DataType::String),
+        Field::new("raw_characters".into(), DataType::UInt64),
+        Field::new("i18n_characters".into(), DataType::UInt64),
+        Field::new("status_code".into(), DataType::String),
     ]);
 
     let lazy_frame = LazyCsvReader::new("values.csv")
@@ -14,12 +14,17 @@ fn main() {
         .finish()
         .unwrap();
 
-    let grouped_df = lazy_frame
-        .group_by([col("raw characters")])
-        .agg([col("status code").sum()])
-        .sort(["raw characters"], Default::default())
+    let mut grouped_df = lazy_frame
+        .group_by(["raw_characters"])
+        .agg([col("raw_characters").count().alias("frequency")])
+        .sort(["raw_characters"], Default::default())
         .collect()
         .unwrap();
+
+    let mut file = File::create("example.csv").expect("could not create file");
+
+    let _ = CsvWriter::new(&mut file)
+        .finish(&mut grouped_df);
 
     println!("{:?}", grouped_df);
 }
